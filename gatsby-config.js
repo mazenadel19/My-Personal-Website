b0704d881f921037f8dd8c034ae8e5c52dee5748
Mazen Emam <mazenadel19@gmail.com>
2023-02-25 13:14:34 +0200

    restore sitemap and add robots txt

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,50 +1,16 @@
-const siteUrl = `https://mazenadel19.vercel.app/`
+
+const siteUrl = `https://mazenadel19.vercel.app/`;
 
 module.exports = {
   siteMetadata: {
     title: `Mazen Emam`,
     description: `Mazen Emam is a software engineer based in Egypt, He is proficient in developing user interfaces with React and other frontend web technologies. He has a good understanding of web development principles, including HTML, CSS, JavaScript, and related frameworks, and is experienced in building interactive, visually appealing web applications. He is also able to efficiently debug and optimize web applications, utilizing debugging tools and performance optimization techniques to ensure a smooth user experience.`,
     author: `Mazen Emam`,
-    // If you didn't use the resolveSiteUrl option this needs to be set
-    siteUrl: `https://mazenadel19.vercel.app/`,
+    siteUrl: `https://mazenadel19.vercel.app`,
   },
   plugins: [
-    {
-      resolve: "gatsby-plugin-sitemap",
-      options: {
-        query: `
-        {
-          allSitePage {
-            nodes {
-              path
-            }
-          }
-          }
-        }
-      `,
-        resolveSiteUrl: () => siteUrl,
-        resolvePages: ({
-          allSitePage: { nodes: allPages },
-        }) => {
-          const wpNodeMap = allWpNodes.reduce((acc, node) => {
-            const { uri } = node;
-            acc[uri] = node;
-
-            return acc;
-          }, {});
-
-          return allPages.map((page) => {
-            return { ...page, ...wpNodeMap[page.path] };
-          });
-        },
-        serialize: ({ path, modifiedGmt }) => {
-          return {
-            url: path,
-            lastmod: modifiedGmt,
-          };
-        },
-      },
-    },
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-robots-txt`,
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
