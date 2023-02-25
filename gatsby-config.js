955ad8d266c92baa953d0b693fd01678bff90f40
Mazen Emam <mazenadel19@gmail.com>
2023-02-25 13:05:30 +0200

    remove allWpContentNode

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,24 +19,12 @@ module.exports = {
               path
             }
           }
-          allWpContentNode(filter: {nodeType: {in: ["Post", "Page"]}}) {
-            nodes {
-              ... on WpPost {
-                uri
-                modifiedGmt
-              }
-              ... on WpPage {
-                uri
-                modifiedGmt
-              }
-            }
           }
         }
       `,
         resolveSiteUrl: () => siteUrl,
         resolvePages: ({
           allSitePage: { nodes: allPages },
-          allWpContentNode: { nodes: allWpNodes },
         }) => {
           const wpNodeMap = allWpNodes.reduce((acc, node) => {
             const { uri } = node;
