695b8ac1b552083a91a21f8f5df9d899b02842a4
Mazen <mazenadel19@gmail.com>
2024-09-25 21:46:56 +0000

    feat: add ceasefire banner

---
+++ src/components/banners/ceasefire/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./index.css"
+
+export default function CeaseFire() {
+  return (
+    <div className="banner">
+      <a href="https://ceasefiretoday.com/" target="_blank" rel="noreferrer" referrerPolicy="no-referrer">
+      # CeasefireNow! <span role="img" aria-label="palestine emoji">🇵🇸</span></a>
+    </div>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,15 @@ import {
   ProjectsSection,
   Seo,
 } from "gatsby-theme-portfolio-minimal";
+import Banner from '../components/banners/ceasefire'
 
 import "./index.css"
-
 
 export default function IndexPage() {
   return (
     <>
       <Seo title="Mazen Emam" />
+      <Banner/>
       <Page useSplashScreenAnimation>
         <HeroSection sectionId="hero" />
         <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} />
